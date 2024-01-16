d023095a30710c7fb7d3eee1d60b32ee53017bb5
Nikhil-tr <152260396+Nikhil-tr@users.noreply.github.com>
2024-01-16 15:42:51 +0530

    Create kube-client.sh

---
+++ kube-client.sh
@@ -0,0 +1,18 @@
+## Install Docker
+sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/scripts/installDocker.sh -P /tmp
+sudo chmod 755 /tmp/installDocker.sh
+sudo bash /tmp/installDocker.sh
+sudo systemctl restart docker.service
+
+## Install CRI-Docker
+sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/scripts/installCRIDockerd.sh -P /tmp
+sudo chmod 755 /tmp/installCRIDockerd.sh
+sudo bash /tmp/installCRIDockerd.sh
+sudo systemctl restart cri-docker.service
+
+## Install kubeadm,kubelet,kubectl
+sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/scripts/installK8S.sh -P /tmp
+sudo chmod 755 /tmp/installK8S.sh
+sudo bash /tmp/installK8S.sh
+
+##sudo |token| --cri-socket unix:///var/run/cri-dockerd.sock
