9601f41684352b7da42fd8eaaf0349127cafa371
Nikhil-tr <152260396+Nikhil-tr@users.noreply.github.com>
2024-01-16 15:37:14 +0530

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,5 @@
+## Install Docker
+sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/scripts/installDocker.sh -P /tmp
+sudo chmod 755 /tmp/installDocker.sh
+sudo bash /tmp/installDocker.sh
+sudo systemctl restart docker.service
