ff5323c84630c08ad6ab3b8fbe3fcf7a6d46c314
Nikhil-tr <152260396+Nikhil-tr@users.noreply.github.com>
2024-01-16 15:52:37 +0530

    Update terraform.sh

--- terraform.sh
+++ terraform.sh
@@ -1,3 +1,3 @@
-sudo curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add - -y
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main" -y
 sudo apt-get update && sudo apt-get install terraform -y
