cd4125075551927209b3296bf525782111d45a7f
Nikhil-tr <152260396+Nikhil-tr@users.noreply.github.com>
2024-03-14 16:52:42 +0530

    Update java.sh

--- java.sh
+++ java.sh
@@ -1,6 +1,4 @@
-sudo apt update -y
-sudo apt install fontconfig openjdk-17-jre -y
+sudo apt update
+sudo apt install fontconfig openjdk-17-jre
 java -version
 openjdk version "17.0.8" 2023-07-18
-OpenJDK Runtime Environment (build 17.0.8+7-Debian-1deb12u1)
-OpenJDK 64-Bit Server VM (build 17.0.8+7-Debian-1deb12u1, mixed mode, sharing)
