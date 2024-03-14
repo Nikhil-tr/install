fc75155b2c557882c9e3699f869585d9e2e1916f
Nikhil-tr <152260396+Nikhil-tr@users.noreply.github.com>
2024-03-14 16:51:23 +0530

    Create java.sh

---
+++ java.sh
@@ -0,0 +1,6 @@
+sudo apt update -y
+sudo apt install fontconfig openjdk-17-jre -y
+java -version
+openjdk version "17.0.8" 2023-07-18
+OpenJDK Runtime Environment (build 17.0.8+7-Debian-1deb12u1)
+OpenJDK 64-Bit Server VM (build 17.0.8+7-Debian-1deb12u1, mixed mode, sharing)
